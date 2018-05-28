957cccad1d52ff8437ddf043b3e78b6307e1dde2
smythinator <chris.smythe30@gmail.com>
2018-05-28 15:57:08 -0700

    Refector API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todoNextId = 1;
@@ -35,13 +37,13 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo;
+  var matchedTodo = _.findWhere(todos, {id: todoId});
 
-  todos.forEach(function(todo) {
-    if (todoId === todo.id) {
-      matchedTodo = todo;
-    }
-  });
+  // todos.forEach(function(todo) {
+  //   if (todoId === todo.id) {
+  //     matchedTodo = todo;
+  //   }
+  // });
 
   if (matchedTodo) {
     res.json(matchedTodo);
@@ -52,7 +54,17 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-  var body = req.body;
+  // use _.pick to restrict submitted fields to only description and completed
+  var body = _.pick(req.body, 'description', 'completed');
+
+  // validate body field entries
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400); // req can't be completed bc bad data was provided
+  }
+
+  // remove unnecessary spaces from body field
+  body.description = body.description.trim();
+
   // add id field
   body.id = todoNextId++;
   todos.push(body);

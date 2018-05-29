689647de9d4086b8aa4fc562f286e093d7e0b48a
smythinator <chris.smythe30@gmail.com>
2018-05-28 17:16:41 -0700

    Final static to-dos

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/', function(req, res) {
   res.send("Todo API Root");
 });
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=myKeyWord
 app.get('/todos', function(req, res) {
   var queryParams = req.query;
   var filteredTodos = todos;
@@ -42,6 +42,11 @@ app.get('/todos', function(req, res) {
     filteredTodos = _.where(filteredTodos, {completed: false});
   }
 
+  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    filteredTodos = _.filter(filteredTodos, function (todo) {
+      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    });
+  }
   res.json(filteredTodos);
 });
 // GET /todos/:id
